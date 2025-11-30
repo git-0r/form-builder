85c930e2a89d6da8d9179a9775d3027b8a3803e3
git-0r <rohitdec97@gmail.com>
2025-12-01 01:00:56 +0530

    fix: increase rate limit to 1500/15min

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -36,7 +36,7 @@ app.use(
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes window
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1500, // Limit each IP to 1500 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
